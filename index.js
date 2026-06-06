79c73a6b604d25dad4992b23063fdc3ba7753b16
rafapaezbas <rafa@holepunch.to>
2026-06-07 01:10:18 +0200

    list update

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const { Program, sequence, quit, key, filepicker, style, viewport, spinner, list } = require('@holepunchto/bare-tui')
+const {
+  Program,
+  // sequence,
+  quit,
+  key,
+  filepicker,
+  style,
+  // viewport,
+  spinner,
+  list
+} = require('@holepunchto/bare-tui')
 const { filterMp3Files } = require('./lib/utils.js')
 
 const constants = {
-  FP_PANEL : 0,
+  FP_PANEL: 0,
   PREVIEW_PANEL: 1
 }
 
@@ -15,8 +25,14 @@ class Preview {
     })
   }
 
-  view () {
-    return this.list.view()
+  view(width, height) {
+    return style().width(width).height(height).render(this.list.view())
+  }
+
+  update(msg) {
+    const [l, cmd] = this.list.update(msg)
+    this.list = l
+    return cmd
   }
 }
 
@@ -46,12 +62,12 @@ class App {
       return [this, null]
     }
     if (msg.type === 'filepicker.entries') {
-      this._updatePreview(msg.dir)
+      this._setPreviewItems(msg.dir)
       // return [this, null]
     }
     if (msg.type === 'key' && key.matches(msg, 'q', 'ctrl+c')) {
       return [this, quit]
-    } 
+    }
 
     if (msg.type === 'resize') {
       this.width = msg.width
@@ -68,14 +84,20 @@ class App {
       return this._updateSpinner(msg)
     }
 
-    if (this.selectedPanel % this.pannels === constants.FP_PANEL) { // fp active
+    if (this.selectedPanel % this.pannels === constants.FP_PANEL) {
+      // fp active
       return this._updateFp(msg)
+    }
+
+    if (this.selectedPanel % this.pannels === constants.PREVIEW_PANEL) {
+      // preview active
+      return this.preview.update(msg)
     }
 
     return [this, null]
   }
 
-  _updateFp (msg) {
+  _updateFp(msg) {
     const [fp, cmd] = this.fp.update(msg)
     this.fp = fp
     return [this, cmd]
@@ -87,7 +109,7 @@ class App {
     return [this, cmd]
   }
 
-  _updatePreview(path) {
+  _setPreviewItems(path) {
     this.preview.list.setItems(filterMp3Files(path))
   }
 
@@ -100,33 +122,29 @@ class App {
       .height(this.height - this.bottomPadding)
       .render(this.fp.view())
 
-    const spinner = style()
-      .bold(true)
-      .foreground('green')
-      .render(this.spinner.view())
+    const spinner = style().bold(true).foreground('green').render(this.spinner.view())
 
     const preview = style()
       .border(style.borders.rounded)
-      .borderForeground(this.selectedPanel % this.pannels === constants.PREVIEW_PANEL ? 'red' : 'blue')
+      .borderForeground(
+        this.selectedPanel % this.pannels === constants.PREVIEW_PANEL ? 'red' : 'blue'
+      )
       .padding(0, 1)
       .width(this.width / 2)
       .height(this.height - this.bottomPadding)
-    // .render(spinner + ' hello tui!')
-      .render(this.preview.view())
+      // .render(spinner + ' hello tui!')
+      .render(
+        this.preview.list.items.length
+          ? this.preview.view(this.width / 2, this.height - this.bottomPadding)
+          : 'No items'
+      )
 
     const body = style.joinHorizontal(style.position.top, fp, ' ', preview)
 
-    const footer = this.picked
-      ? `  ✓ picked: ${this.picked}`
-      : '  ↑/↓ move · ↵/→ open · ⌫/← up · q quit '
+    const footer = ['  ↑/↓ move · ↵/→ open · ⌫/← up · q quit ']
 
-    return style.joinVertical(
-      style.position.left,
-      body,
-      footer
-    )
+    return style.joinVertical(style.position.left, body, ' ', footer[this.selectedPanel])
   }
 }
 
 new Program(new App()).run()
-
